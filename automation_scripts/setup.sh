3d9ea5740c7876cb48aa7a88a0006434ecc29838
Ofentse Sibande <0512225143084@tpc.local>
2025-07-28 12:51:52 +0200

    First Commit

---
+++ automation_scripts/setup.sh
@@ -0,0 +1,8 @@
+mkdir project_folder
+cd project_folder
+mkdir src
+mkdir docs
+cd src
+touch index.js
+echo "console.log('Project started');" > project_folder/src/index.js
+echo "✅ Folder structure created successfully!"
